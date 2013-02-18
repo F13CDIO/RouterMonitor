24576b2871288633809b370823a25e6dc8efae30
Mads Slotsbo <mads.slotsbo@gmail.com>
2013-02-18 17:58:09 +0100

    test

--- 18_02324_F13/D1/Main/Main.java
+++ 18_02324_F13/D1/Main/Main.java
@@ -15,7 +15,7 @@ public class Main
 		{//TODO Vi har et problem med kode, da vi får en tilfældig kode.. 
 			users.createUser(new User(1, "", "", "", Rights.Student));
 			users.getUser(10);
-			users.updateUser(new User(11, "", "", "", Rights.Teacher));
+			users.updateUser(new User(1, "", "", "", Rights.Teacher));
 			users.updateUserPassword(new User(19, "", "", "ReRFdf!hH", Rights.Admin));
 		}
 		catch(DALException e)
