e75a2314d7fc5b5b684091603e031b1f34e43062
Mads Slotsbo <mads.slotsbo@gmail.com>
2013-03-01 16:09:57 +0100

    kommentarer og tilgængelighed af funktionens data

--- 18_02324_F13/server/Data/DataTest.java
+++ 18_02324_F13/server/Data/DataTest.java
@@ -4,8 +4,7 @@ import Funktion.*;
 
 public class DataTest 
 {
-	IData data = new Data();
-	IParseUdpPackage parse  = new ParseUdpPackage();;
+	private IParseUdpPackage parse  = new ParseUdpPackage();;
 	public static void main(String[] args)
 	{
 		DataTest testClass = new DataTest();
@@ -14,7 +13,14 @@ public class DataTest
 	
 	public void funktionTest()
 	{
-		parse.parse("T 10.16.99.136:56086 -> 173.194.65.139:80 [AP] \n"+
-				 " POST /safebrowsing/downloads?pver=2.2&client=Safari&appver=6.0.2 HTTP/1.1..Host: safebrowsing.clients.google.com..User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_7_5) AppleWebKit/536.26.17 (KHTML, li");
+		String test1 = "T 10.1699.13655751 -> 69.171.235.1680 [AP]\n GET /ping?partition=236&cb=gks9 HTTP/1.1..Hot: 3-pct.channel.facebook.com..Connection: keep-alive..User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_7_5) AppleWebKit/537.22 (KHTML, like Gecko) Chrome/";
+		String test2 = "T 10.1699.136:55751 -> 69.171.235.16:80 [AP]\n GET /ping?partition=236&cb=gks9 HTTP/1.1..Host: 3-pct.channel.facebook.com..Connection: keep-alive..User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_7_5) AppleWebKit/537.22 (KHTML, like Gecko) Chrome/";
+		parse.parse(test2);
+		parse.parse(test1);
+		
+        System.out.println("IP1 = " + parse.getData().getInIP(0));
+        System.out.println("IP2 = " + parse.getData().getOutIP(0));
+        System.out.println("Host = " + parse.getData().getHost(0));
+        System.out.println("Subhost = " + parse.getData().getSubHost(0));
 	}
 }
--- 18_02324_F13/server/Funktion/ParseUdpPackage.java
+++ 18_02324_F13/server/Funktion/ParseUdpPackage.java
@@ -2,60 +2,108 @@ package Funktion;
 
 import Data.*;
 
-public class ParseUdpPackage implements IParseUdpPackage {
-    int index;
-    IData data;
+public class ParseUdpPackage implements IParseUdpPackage
+{
+    private int index;
+    private IData data;
     
     public ParseUdpPackage()
     {
     	data = new Data();
     }
 	
-    public void parse(String input) {
+    //Lave til public void parse(String input, IData data) i stedet for?, for at en controller kan bruge samme dataobject 
+    public void parse(String input) 
+    {
         String ip1 = getIP1(input);
         String ip2 = getIP2(input);
         
         String fullHost = getFullHost(input);
         String host = getHost(fullHost);
         String subHost = getSubHost(fullHost);
-        data.addDataset(ip1, ip2, host, subHost, null);
         
-        System.out.println("IP1 = " + data.getInIP(0));
-        System.out.println("IP2 = " + data.getOutIP(0));
-        System.out.println("Host = " + data.getHost(0));
-        System.out.println("Subhost = " + data.getSubHost(0));
+        if(ip1 != null && ip2 != null && host != null)
+        {
+        	data.addDataset(ip1, ip2, host, subHost, null);
+        }
     }
 
-    private String getIP1(String input) {
+    private String getIP1(String input)
+    {
         index = input.indexOf(":", 9);
-        String ip = input.substring(2, index);
-        return ip;
+        if(index < 0)
+        {
+        	return null;
+        }
+        else
+        {
+        	String ip = input.substring(2, index);
+            return ip;
+        }
     }
 
-    private String getIP2(String input) {
+    private String getIP2(String input)
+    {
         index = input.indexOf(" ", index);
         int pos = input.indexOf(":", (index) + 11);
-        String ip = input.substring(index + 4, pos);
-        return ip;
+        if(pos < 0 || index < 0)
+        {
+        	return null;
+        }
+        else
+        {
+        	String ip = input.substring(index + 4, pos);
+            return ip;
+        }
     }
     
-    private String getFullHost(String input) {
+    private String getFullHost(String input)
+    {
         index = input.indexOf("Host: ", index);
         int pos = input.indexOf("..", index);
-        String host = input.substring(index+6, pos);
-        return host;
+        if(pos < 0 || index < 0)
+        {
+        	return " ";
+        }
+        else
+        {
+        	String host = input.substring(index + 6, pos);
+            return host;
+        }
     }
     
-    private String getHost(String fullHost) {
+    private String getHost(String fullHost)
+    {
         int pos = fullHost.lastIndexOf(".");
         index = fullHost.lastIndexOf(".", pos-1);
-        String host = fullHost.substring(index+1);
-        return host;
+        if(pos < 0 )
+        {
+        	return null;
+        }
+        else
+        {
+            String host = fullHost.substring(index+1);
+            return host;
+        }
     }
     
-    private String getSubHost(String fullHost) {
+    private String getSubHost(String fullHost)
+    {
         int pos = fullHost.lastIndexOf(".", index-1);
-        String subHost = fullHost.substring(pos+1, index);
-        return subHost;
+        if(pos < 0)
+        {
+        	return null;
+        }
+        else
+        {
+	        String subHost = fullHost.substring(pos+1, index);
+	        return subHost;
+        }
+    }
+    
+    //Bør denne metode være der??
+    public IData getData()
+    {
+    	return data;
     }
 }
