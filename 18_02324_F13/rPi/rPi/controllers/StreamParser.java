fdda21ba574a7694c6ad7bbdbbcfd88649037540
Bjqrn88 <andersbjorn88@gmail.com>
2013-03-04 11:47:09 +0100

    parse

--- 18_02324_F13/rPi/rPi/controllers/StreamParser.java
+++ 18_02324_F13/rPi/rPi/controllers/StreamParser.java
@@ -2,14 +2,35 @@ package rPi.controllers;
 
 import java.io.BufferedReader;
 
+
+
 public class StreamParser {
 	
-	public String parseString(String str){
+	String trigger = "T";
+	
+	public String parseString(String str)
+	{
+		
+		String lines = "";
+		
+		while(str != null)
+		{
+			if(str.startsWith(trigger))
+			{
+				for (int i = 0; i < 2; i++) 
+				{
+					lines = str+lines+"\n";
+				}
+			}
+		}
+		
 		return str;
 	}
-	public String parseNetworkData(String str){
+	public String parseNetworkData(String str)
+	{
 		return str;
 	}
+	
 //	public String parsePing(String str) 
 //	{
 //		return str;
