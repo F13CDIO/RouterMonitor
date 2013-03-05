6de992e5e73cfa36e475c71d677fb232cda8802f
Chris Johansen <chrisdons@gmail.com>
2013-03-05 17:57:28 +0100

    Større intern optimering af ParseUdpPackage..
    - Ændret "getUserAgent" til privat
    - Lavet getUserAgent langt mere robust, så den ikke giver exeption ved
    "støj"-input
    - Tilføjet ekstra tjek til alle de oprindelige get-metoder, så de er
    mere robuste
    - Ændret tjek før tilføjelse af data, så info om en pakke gemmes selvom
    useragent ikke bliver fundet
    - Optimeret så der spares en String-kopiering
    - Mindre ændringer i formateringen generelt
    - Tilføjet kommentarer

--- 18_02324_F13/server/Funktion/ParseUdpPackage.java
+++ 18_02324_F13/server/Funktion/ParseUdpPackage.java
@@ -3,116 +3,110 @@ package Funktion;
 import Data.*;
 import java.util.Calendar;
 
-public class ParseUdpPackage implements IParseUdpPackage
-{
-    private int index;
+public class ParseUdpPackage implements IParseUdpPackage {
+
+    private int masterIndex;
     private IData data = new DataLayer().getData();
-    
-    public ParseUdpPackage()
-    {
+
+    public ParseUdpPackage() {
     }
-	
-    public void parse(String input) 
-    {
-    	Calendar cal = Calendar.getInstance();
+
+    public void parse(String input) {
+        Calendar cal = Calendar.getInstance();
+
+        /* Different 'get'-methods for the different types of info.
+         * It is important that methods run in the right order, since they share
+         * the same masterIndex variable, for better performance */
         String ip1 = getIP1(input);
         String ip2 = getIP2(input);
-        
-        String fullHost = getFullHost(input);
-        String host = getHost(fullHost);
-        String subHost = getSubHost(fullHost);
+        String subHost = getSubHost(input);
+        String host = getHost(input);
         String userAgent = getUserAgent(input);
-        
-        if(ip1 != null && ip2 != null && host != null)
-        {
-        	data.addDataset(cal.getTime(), ip1, ip2, host, subHost, userAgent);
+
+        /* Only save data if all the "important" information was found.
+         * Useragent and subhost is not considered "important" */
+        if (ip1 != null && ip2 != null && host != null) {
+            data.addDataset(cal.getTime(), ip1, ip2, host, subHost, userAgent);
         }
     }
 
-    private String getIP1(String input)
-    {
-        index = input.indexOf(":", 9);
-        if(index < 0)
-        {
-        	return null;
+    private String getIP1(String input) {
+        masterIndex = input.indexOf(":", 9);
+
+        if (masterIndex < 2) {
+            return null;
         }
-        else
-        {
-        	String ip = input.substring(2, index);
-            return ip;
+        else {
+            return input.substring(2, masterIndex);
         }
     }
 
-    private String getIP2(String input)
-    {
-        index = input.indexOf(" ", index);
-        int pos = input.indexOf(":", (index) + 11);
-        if(pos < 0 || index < 0)
-        {
-        	return null;
+    private String getIP2(String input) {
+        int endIndex;
+
+        masterIndex = input.indexOf(" ", masterIndex);
+        endIndex = input.indexOf(":", (masterIndex) + 11);
+
+        if (endIndex < 0 || masterIndex < 0 || (masterIndex + 4) > endIndex) {
+            return null;
         }
-        else
-        {
-        	String ip = input.substring(index + 4, pos);
-            return ip;
-        }
-    }
-    
-    private String getFullHost(String input)
-    {
-        index = input.indexOf("Host: ", index);
-        int pos = input.indexOf("..", index);
-        if(pos < 0 || index < 0)
-        {
-        	return " ";
-        }
-        else
-        {
-        	String host = input.substring(index + 6, pos);
-            return host;
+        else {
+            return input.substring(masterIndex + 4, endIndex);
         }
     }
-    
-    private String getHost(String fullHost)
-    {
-        int pos = fullHost.lastIndexOf(".");
-        index = fullHost.lastIndexOf(".", pos-1);
-        if(pos < 0 )
-        {
-        	return null;
+
+    private String getSubHost(String input) {
+        int hostIndex, dotIndex;
+
+        hostIndex = input.indexOf("Host: ", masterIndex);
+        masterIndex = input.indexOf("..", hostIndex);
+        masterIndex = input.lastIndexOf(".", masterIndex - 1);
+        masterIndex = input.lastIndexOf(".", masterIndex - 1);
+
+        /* If there is no dot between the index if "Host" and the index of
+         * the last dot in the domain, there is no subdomain, so return null.
+         * Else find the index of the last dot in that range */
+        if (masterIndex < hostIndex || hostIndex < 0) {
+            masterIndex = hostIndex + 5;
+            return null;
         }
-        else
-        {
-            String host = fullHost.substring(index+1);
-            return host;
+        else {
+            dotIndex = input.lastIndexOf(".", masterIndex - 1);
+
+            /* If there is no dot between "Host" and the main domain, then
+             * the index to cut from, will just be the index of the end of "Host" */
+            if (hostIndex > dotIndex) {
+                dotIndex = hostIndex + 5;
+            }
+
+            return input.substring(dotIndex + 1, masterIndex);
         }
     }
-    
-    private String getSubHost(String fullHost)
-    {
-        int pos = fullHost.lastIndexOf(".", index-1);
-        if(pos < 0)
-        {
-        	return null;
+
+    private String getHost(String input) {
+        int endIndex;
+
+        endIndex = input.indexOf("..", masterIndex);
+
+        if (masterIndex - 5 < 0 || endIndex < 0 || (masterIndex + 1) > endIndex) {
+            return null;
         }
-        else
-        {
-	        String subHost = fullHost.substring(pos+1, index);
-	        return subHost;
+        else {
+            return input.substring(masterIndex + 1, endIndex);
         }
     }
-    
-    public String getUserAgent(String input){
-    	index = input.indexOf("User-Agent", index);
-    	int endIndex = input.indexOf(")", index);
-    	if(index < 0 || endIndex < 0)
-    	{
-    		return null;
-    	}
-    	else
-    	{
-	    	String userAgent = input.substring(index+12,endIndex+1);
-	    	return userAgent;
-    	}
+
+    private String getUserAgent(String input) {
+        int endIndex;
+
+        masterIndex = input.indexOf("User-Agent", masterIndex);
+        endIndex = input.indexOf(")", masterIndex);
+
+        if (masterIndex < 0 || endIndex < 0 || (masterIndex + 12) > (endIndex + 1)) {
+            return null;
+        }
+        else {
+            return input.substring(masterIndex + 12, endIndex + 1);
+        }
     }
 }
