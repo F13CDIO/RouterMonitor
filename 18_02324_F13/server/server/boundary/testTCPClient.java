a6ebcd68206b281311b0da5495776eb5a61f1f3e
redlaz <lasse@redlaz.dk>
2013-03-18 16:35:45 +0100

    test udp og tcp

---
+++ 18_02324_F13/server/server/boundary/testTCPClient.java
@@ -0,0 +1,46 @@
+package server.boundary;
+	
+	import java.io.*;
+	import java.net.*;
+
+	
+
+	class testTCPClient
+	{
+		private Socket clientSocket;
+		private BufferedReader inFromServer;
+		private DataOutputStream outToServer;
+		private String serverAddress = "127.0.0.1";
+		private int serverPort = 9000;
+
+		public testTCPClient() throws Exception
+		{
+			clientSocket = new Socket(serverAddress, serverPort);
+			inFromServer = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));	
+		}
+		
+		public void sendLine(String message)
+		{
+			try {
+				outToServer = new DataOutputStream(clientSocket.getOutputStream());
+				outToServer.writeBytes(message);
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}	
+		}
+		
+		public void closeConnection() throws Exception
+		{
+			try {
+				clientSocket.close();
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+	}
+	
+	
+	
+
