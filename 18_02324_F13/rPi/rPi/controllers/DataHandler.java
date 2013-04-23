2459b7a11a4fbe21f6b18fc5bd4b0a8bdce5a430
Jacob espersen <jacobespersen@gmail.com>
2013-04-23 17:00:36 +0200

    pie med ny kommando

--- 18_02324_F13/rPi/rPi/controllers/DataHandler.java
+++ 18_02324_F13/rPi/rPi/controllers/DataHandler.java
@@ -7,7 +7,7 @@ public class DataHandler {
 	TerminalExecutor tc = new TerminalExecutor();
 	// The command run in the script is "ngrep -q '^(GET|POST|HEAD|CONNECT)' 'tcp'"
 	// the following command requires ngrep opened in sudoers file or the java program executed with su rights
-	String command = "/opt/local/bin/ngrep -q &";
+	String command = "/Applications/Wireshark.app/Contents/Resources/bin/tshark -T fields -e ip.src -e ip.dst -e http.host -e http.user_agent -i en1 -I tcp port 80 and ip";
 	public BufferedReader br = null;
 	
 	public BufferedReader startSniffing(){
