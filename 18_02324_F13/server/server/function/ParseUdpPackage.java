16bef94ef922a5e175aa73b28c6f3c79ad93887a
Mads Slotsbo <mads.slotsbo@gmail.com>
2013-04-29 17:30:55 +0200

    småoptimeringer

--- 18_02324_F13/server/server/function/ParseUdpPackage.java
+++ 18_02324_F13/server/server/function/ParseUdpPackage.java
@@ -8,12 +8,14 @@ public class ParseUdpPackage implements IParseUdpPackage, IFunction {
 
 	//A number defining where in the string we are looking
     private IData data;
-    private int dataAdded;
+    private String userAgent;
+    private String host;
+    private String distinationIP;
+    private String sourceIP;
 
     public ParseUdpPackage() 
     {
     	data = Function.getDatalayer();
-    	dataAdded = 0;
     }
 
     public void parse(String input) 
@@ -24,22 +26,31 @@ public class ParseUdpPackage implements IParseUdpPackage, IFunction {
         /* Different 'get'-methods for the different types of info.
          * It is important that methods run in the right order, since they share
          * the same masterIndex variable, for better performance */
-        String host;
-        if(!sniffedData[2].toLowerCase().contains("dropbox"))
+        if(!sniffedData[2].toLowerCase().contains("dropbox")) //fjerner dropbox
         	host = sniffedData[2];
         else
         	return;
-        String sourceIP = sniffedData[0];
-        String distinationIP = sniffedData[1];
-        String userAgent = sniffedData[3].replaceAll("\0", "");
+        sourceIP = sniffedData[0];
+        distinationIP = sniffedData[1];
+        
+    	//removes avast and steam, and more?
+        if(!sniffedData[3].toLowerCase().contains("Avast")||!sniffedData[3].toLowerCase().contains("steam")) //Optimize
+        {	
+        	userAgent = sniffedData[3].replaceAll("\0", "");
+        	if(userAgent.length() > 300)
+        	{
+        		return;
+        	}
+        }
+        else
+        {
+        	return;
+        }
 
         /* Only save data if all the "important" information was found.
          * Useragent and subhost is not considered "important" */
         if (sourceIP != null && distinationIP != null && host != null) {
             data.addDataset(cal.getTime(), sourceIP, distinationIP, host, userAgent);
-            //System.out.println(data.getDataPackage().toString());
-            dataAdded++;
-            //System.out.println("Package count: " + dataAdded);
             
         }
     }
