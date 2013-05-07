db0b0ac64ac1fa02b3ddb26e71a662211f6fc4ff
Bjqrn88 <andersbjorn88@gmail.com>
2013-05-07 16:28:45 +0200

    mere web

--- 18_02324_F13/wb/logbean/connect.java
+++ 18_02324_F13/wb/logbean/connect.java
@@ -7,7 +7,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-public class connect extends HttpServlet {
+public class connect extends HttpServlet{
 
 
 	private static final long serialVersionUID = 1L;
