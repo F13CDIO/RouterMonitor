61abed41714a10ceb2ebae8c45bfabfe27d8e048
Mads Slotsbo <mads.slotsbo@gmail.com>
2013-05-13 13:43:49 +0200

    lukning af filereader

--- 18_02324_F13/server/server/function/RegexFromFile.java
+++ 18_02324_F13/server/server/function/RegexFromFile.java
@@ -3,6 +3,7 @@ package server.function;
 import java.io.BufferedReader;
 import java.io.DataInputStream;
 import java.io.FileInputStream;
+import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class RegexFromFile
@@ -11,21 +12,38 @@ public class RegexFromFile
 	 * 
 	 * @param file Can be "userAgent" or "host" 
 	 * @return Returns the first line of the file
+	 * @throws IOException if it can't close in finally
 	 */
 	public String ReadLine(String file)
 	{
 		String line = null;
+		
+		FileInputStream stream = null;
+		DataInputStream in = null;
+		BufferedReader br = null;
 		try
 		{
-			FileInputStream stream = new FileInputStream("RegEx files/"+file);
-			DataInputStream in = new DataInputStream(stream);
-			BufferedReader br = new BufferedReader(new InputStreamReader(in));
+			stream = new FileInputStream("RegEx files/"+file);
+			in = new DataInputStream(stream);
+			br = new BufferedReader(new InputStreamReader(in));
 			line = br.readLine(); //reads the first line
-			br.close();
 		}
-		catch(Exception e)
+		catch(IOException e)
 		{
 			System.out.println("file " +file+ ".txt not found");
+		}
+		finally
+		{
+			try//try for at lukke?? alternativt er throws, men så skal der try længere oppe
+			{
+				br.close();
+				in.close();
+				stream.close();
+			}
+			catch(IOException e)
+			{
+				System.out.println(e.getMessage());
+			}
 		}
 		return line;
 	}
