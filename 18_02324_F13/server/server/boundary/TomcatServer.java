578bc59b973ba3845be2445cf1bd89a07511f4ea
Mads Slotsbo <mads.slotsbo@gmail.com>
2013-06-14 19:55:57 +0200

    Server setup

--- 18_02324_F13/server/server/boundary/TomcatServer.java
+++ 18_02324_F13/server/server/boundary/TomcatServer.java
@@ -2,9 +2,13 @@ package server.boundary;
 
 import java.io.File;
 
+import javax.servlet.ServletException;
+
 import org.apache.catalina.Context;
 import org.apache.catalina.LifecycleException;
 import org.apache.catalina.startup.Tomcat;
+
+import server.main.MainServer;
 
 
 public class TomcatServer
@@ -14,13 +18,20 @@ public class TomcatServer
 	{
 		Tomcat tomcat = new Tomcat();
 		tomcat.setPort(8080);
+		tomcat.enableNaming();
+		
 		File base = new File("WebContent");
 		
 		tomcat.setBaseDir(base.getAbsolutePath());
-		
-		Context rootCtx = tomcat.addContext("", base.getAbsolutePath());
-		
-		Tomcat.initWebappDefaults(rootCtx);
+		try
+		{
+			tomcat.addWebapp("/", base.getAbsolutePath()).getDocBase();
+		}
+		catch (ServletException e1)
+		{
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
 		
 			try
 			{
