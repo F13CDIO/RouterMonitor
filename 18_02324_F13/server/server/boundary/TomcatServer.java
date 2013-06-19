09971db227bf6482b7aa1993154f1e1fe4c31388
Bjqrn88 <andersbjorn88@gmail.com>
2013-06-19 10:36:25 +0200

    Så er HTTPS tilføjet til TomcatServeren

--- 18_02324_F13/server/server/boundary/TomcatServer.java
+++ 18_02324_F13/server/server/boundary/TomcatServer.java
@@ -4,6 +4,8 @@ import java.io.File;
 
 import org.apache.catalina.Context;
 import org.apache.catalina.LifecycleException;
+import org.apache.catalina.Service;
+import org.apache.catalina.connector.Connector;
 import org.apache.catalina.startup.Tomcat;
 
 
@@ -22,6 +24,24 @@ public class TomcatServer
 		File base = new File("WebContent");
 		
 		tomcat.setBaseDir(base.getAbsolutePath());
+		
+		Connector httpsConnector = new Connector();
+		
+		httpsConnector.setPort(443);
+		httpsConnector.setSecure(true);
+		httpsConnector.setScheme("https");
+		httpsConnector.setAttribute("clientAuth", false);
+		httpsConnector.setAttribute("sslProtocol", "TLS");
+		httpsConnector.setAttribute("protocol", "org.apache.coyote.http11.Http11NioProtocol");
+		httpsConnector.setAttribute("SSLEnabled", true);
+		httpsConnector.setAttribute("keystoreFile", base.getAbsolutePath()+"/grp18.cert");
+		httpsConnector.setAttribute("keystorePass", "123123");
+		
+		Service tomcatService = tomcat.getService();
+		tomcatService.addConnector(httpsConnector);
+		
+		Connector defaultConnector = tomcat.getConnector();
+		defaultConnector.setRedirectPort(443);
 		try
 		{
 			Context ctx = tomcat.addWebapp("/", base.getAbsolutePath());
